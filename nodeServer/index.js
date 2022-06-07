d62db3954438127268823ad12bf8cbf269a80819
isha614 <ishamehta614@gmail.com>
2022-06-07 22:04:41 +0530

    server added to client

---
+++ nodeServer/index.js
@@ -0,0 +1,15 @@
+//Node Server which will handle socket io connections
+const io=require('socket.io')(8000)
+const users={};
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',name=>{
+        users[socket.id]=name;
+       socket.broadcast.emit('user-joined',name);
+    })
+
+    socket.on('send',message=>{
+
+        socket.broadcast.emit('recieve',{message,name:users[socket.id]})
+    })
+})
